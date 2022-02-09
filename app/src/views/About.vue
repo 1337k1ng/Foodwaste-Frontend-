6362c9585031f423601f22a69570f39d54cebf89
1337k1ng <madsjohanbech@live.dk>
2022-02-09 10:13:05 +0100

    About- Header menu

--- app/src/views/About.vue
+++ app/src/views/About.vue
@@ -1,5 +1,32 @@
+<script setup> 
+
+import {onMounted} from 'vue'
+
+  onMounted(async ()=> {
+   fetch("https://localhost:5001/api/stores")
+   .then( async response => {
+
+      const data = await response.json();
+
+      if(!response.ok){
+        const error = data.message
+        return Promise.reject(error)
+      }
+
+      console.log(data)
+   })
+   .catch(error => {
+     console.error("There was error", error.message)
+   })
+
+      })
+ </script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+  
+
   </div>
 </template>
